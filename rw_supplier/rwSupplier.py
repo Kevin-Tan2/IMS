a68b9221ce55c12b8541a2106ca46eadf83df4e8
Kevin Tan <kevintan314159@gmail.com>
2021-12-07 19:53:28 +1000

    Add button can now add new row of data in rwSupplier.py

--- rw_supplier/rwSupplier.py
+++ rw_supplier/rwSupplier.py
@@ -37,7 +37,32 @@ class RWSupplier(QtWidgets.QMainWindow):
 
         # set event
         self.resetButton.clicked.connect(self.reset_entries)
+        self.addButton.clicked.connect(self.add_df)
 
     def reset_entries(self):
         self.supplierID.clear()
         self.supplierName.clear()
+
+    def construct_df(self):
+        # construct a new data frame from the QLineEdits
+        print("construct")
+        supplierName = str(self.supplierName.text())
+        supplierID = str(self.supplierID.text())
+
+        df = pd.DataFrame({'Supplier ID': [supplierName],
+                           'Supplier Name': [supplierID]})
+        return df
+
+    def refresh_table(self):
+        print("refresh")
+        # refresh the table whenever data frame has been changed
+        self._model = tableModel(self._df)
+        self.tableView.setModel(self._model)
+
+    def add_df(self):
+        # adding new data into the dataframe
+        print("add")
+        df1 = self._df  # store the current data frame into a temp variable
+        df2 = self.construct_df()
+        self._df = df1.append(df2, ignore_index=True)  # adding new row into the dataframe
+        self.refresh_table()
