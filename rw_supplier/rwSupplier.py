cc1d0ed12cf5c8be2243e74bbd2b5ebb5756e676
Kevin Tan <kevintan314159@gmail.com>
2021-12-07 20:01:49 +1000

    Delete button can now delete table row in rwSupplier.py

--- rw_supplier/rwSupplier.py
+++ rw_supplier/rwSupplier.py
@@ -38,6 +38,7 @@ class RWSupplier(QtWidgets.QMainWindow):
         # set event
         self.resetButton.clicked.connect(self.reset_entries)
         self.addButton.clicked.connect(self.add_df)
+        self.deleteButton.clicked.connect(self.delete_row)
 
     def reset_entries(self):
         self.supplierID.clear()
@@ -45,7 +46,6 @@ class RWSupplier(QtWidgets.QMainWindow):
 
     def construct_df(self):
         # construct a new data frame from the QLineEdits
-        print("construct")
         supplierName = str(self.supplierName.text())
         supplierID = str(self.supplierID.text())
 
@@ -54,15 +54,28 @@ class RWSupplier(QtWidgets.QMainWindow):
         return df
 
     def refresh_table(self):
-        print("refresh")
         # refresh the table whenever data frame has been changed
         self._model = tableModel(self._df)
         self.tableView.setModel(self._model)
 
     def add_df(self):
         # adding new data into the dataframe
-        print("add")
         df1 = self._df  # store the current data frame into a temp variable
         df2 = self.construct_df()
         self._df = df1.append(df2, ignore_index=True)  # adding new row into the dataframe
+        self.refresh_table()
+
+    def delete_row(self):
+        # delete the selected row
+        index_list = []  # to store list of selected rows
+
+        for model_index in self.tableView.selectionModel().selectedRows():
+            index = QtCore.QPersistentModelIndex(model_index)
+            index_list.append(index)  # append the selected indices into index_list
+
+        for index in index_list:  # delete all selected rows
+            tempDf = self._df
+            self._df = tempDf.drop(index.row())  # delete each selected row from the dataframe
+
+        self._df = self._df.reset_index(drop=True)  # reset the index in numerical order
         self.refresh_table()
