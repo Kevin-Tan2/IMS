ca13e8a94e75c6ea3e720f45539971182d440bb7
Kevin Tan <kevintan314159@gmail.com>
2021-12-09 16:06:04 +1000

    User Maintenance Entry ui can be loaded

---
+++ maintenance_entry/userMaintenanceEntry.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QHeaderView, QAbstractItemView
+from PyQt5.QtCore import QAbstractTableModel, QSortFilterProxyModel, Qt
+from tableViewModel import tableModel, load_csv
+import pandas as pd
+import os.path
+from pathlib import Path
+
+
+class UserMaintenance(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        uiFilePath = "./maintenance_entry/userMaintenanceEntry.ui"
+
+        # load the ui file
+        loadUi(uiFilePath, self)
