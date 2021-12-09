19451865cb250de40a93e1e1d5d01656e9e6fbcf
Kevin Tan <kevintan314159@gmail.com>
2021-12-09 15:27:55 +1000

    Side menu bar is added to main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sys
 
-from PyQt5.QtCore import QAbstractTableModel
+from PyQt5.QtCore import QAbstractTableModel, QPropertyAnimation
 from login import login
 from customer_entry import customerEntry
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt
 from PyQt5.uic import loadUi
 import pandas as pd
+import icons
 from pathlib import Path
 
 
@@ -19,7 +20,7 @@ class tableModel(QAbstractTableModel):
     def rowCount(self, parent=None):
         return self._data.shape[0]
 
-    def columnCount(self, parnet=None):
+    def columnCount(self, parent=None):
         return self._data.shape[1]
 
     def data(self, index, role=Qt.DisplayRole):
@@ -54,15 +55,38 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__()
 
         # load the main.ui
+
         loadUi("main.ui", self)
+        self.animation = QPropertyAnimation(self.sideMenuContainer, b"maximumWidth")  # Animate minimumWidht
 
         # set event responses
         self.customerEntryBtn.clicked.connect(lambda: self.display(0))
         self.rwContBtn.clicked.connect(lambda: self.display(1))
         self.rwSupplierBtn.clicked.connect(lambda: self.display(2))
+        self.sideMenuBtn.clicked.connect(self.toggleMenu)
 
     def display(self, index):
         self.stackedWidget.setCurrentIndex(index)
+
+    def toggleMenu(self):
+        # Get current left menu width
+        width = self.sideMenuContainer.width()
+
+        # If minimized
+        if width == 0:
+            # Expand menu
+            newWidth = 228
+        # If maximized
+        else:
+            # Restore menu
+            newWidth = 0
+
+        # Animate the transition
+        self.animation.setDuration(250)
+        self.animation.setStartValue(width)  # Start value is the current menu width
+        self.animation.setEndValue(newWidth)  # end value is the new menu width
+        self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+        self.animation.start()
 
 
 class Controller:
