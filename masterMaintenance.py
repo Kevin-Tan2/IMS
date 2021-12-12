84d62c666f3815a8be9adc4037e93cc7f6809562
Kevin Tan <kevintan314159@gmail.com>
2021-12-12 17:45:58 +1000

    MasterMaintenance can now delete a selected row in the table

--- masterMaintenance.py
+++ masterMaintenance.py
@@ -31,6 +31,7 @@ class MasterMaintenance(QtWidgets.QMainWindow):
         # event responses
         self.resetButton.clicked.connect(self.reset_entries)
         self.addButton.clicked.connect(self.add_df)
+        self.deleteButton.clicked.connect(self.delete_row)
 
     def reset_entries(self):
         pass
@@ -47,4 +48,18 @@ class MasterMaintenance(QtWidgets.QMainWindow):
         # adding new data into the dataframe
         tempDf = self.df  # store the current data frame into a temp variable
         self.df = tempDf.append(df, ignore_index=True)  # adding new row into the dataframe
+        self.refresh_table()
+
+    def delete_row(self):
+        # delete the selected row
+        index_list = []  # to store list of selected rows
+
+        for model_index in self.tableView.selectionModel().selectedRows():
+            index = QtCore.QPersistentModelIndex(model_index)
+            index_list.append(index)  # append the selected indices into index_list
+
+        for index in index_list:  # delete all selected rows
+            tempDf = self.df
+            self.df = tempDf.drop(index.row())  # delete each selected row from the dataframe
+
         self.refresh_table()
