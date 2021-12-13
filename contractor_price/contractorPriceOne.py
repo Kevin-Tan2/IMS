cfda3f1424fb4a8db10d1ad08c932a081ba7e2b9
Kevin Tan <kevintan314159@gmail.com>
2021-12-13 15:30:15 +1000

    ContractorPriceOne can now add data to each respective table

--- contractor_price/contractorPriceOne.py
+++ contractor_price/contractorPriceOne.py
@@ -10,33 +10,31 @@ class ContractorPriceOne(MasterMaintenance):
 
     def __init__(self):
         self.uiFilePath = "./contractor_price/contractorPrice.ui"
-        self.csvFilePathOne = "./contractor_price/contractorPriceWetWood.csv"
-        self.csvFilePathTwo = "./contractor_price/contractorPriceDryWood.csv"
+        csvFilePathOne = "./contractor_price/contractorPriceWetWood.csv"
+        csvFilePathTwo = "./contractor_price/contractorPriceDryWood.csv"
+
+        self.csvFilePathArray = [csvFilePathOne, csvFilePathTwo]
 
         self.columnNames = ['Size1', 'Size2', 'Length', 'Price']
 
-        super().__init__(self.uiFilePath, self.csvFilePathOne, self.columnNames)
+        super().__init__(self.uiFilePath, csvFilePathOne, self.columnNames)
 
         # create second dataframe for the second table
-        self.df2 = load_csv(self.csvFilePathTwo, self.columnNames)
+        self.df2 = load_csv(csvFilePathTwo, self.columnNames)
 
         self.model2 = tableModel(self.df2)
 
-        self.tableView2.horizontalHeader().setStretchLastSection(True)  # to stretch the header size to fit
-        self.tableView2.setSelectionBehavior(QAbstractItemView.SelectRows)  # to select entire row instead of cell
-        self.tableView2.setModel(self.model2)  # to set the proxy model into the table view
-
-        # event response for table wet wood
-        self.resetButton.clicked.connect(lambda: self.reset_entries(0))
+        self.dryTableView.horizontalHeader().setStretchLastSection(True)  # to stretch the header size to fit
+        self.dryTableView.setSelectionBehavior(QAbstractItemView.SelectRows)  # to select entire row instead of cell
+        self.dryTableView.setModel(self.model2)  # to set the proxy model into the table view
 
         # event responses for table dry wood
         self.dryResetBtn.clicked.connect(lambda: self.reset_entries(1))
-        self.dryAddBtn.clicked.connect(self.add_df)
+        self.dryAddBtn.clicked.connect(lambda: self.add_df(1))
         self.dryDeleteBtn.clicked.connect(self.delete_row)
-        self.drySaveBtn.clicked.connect(self.save_csv)
-        self.drySearchBar.textChanged.connect(self.search_query)
+        self.drySaveBtn.clicked.connect(lambda: self.save_csv(1))
 
-    def reset_entries(self, tableNo):
+    def reset_entries(self, tableNo=0):
 
         if tableNo == 0:
             self.wetHeight.clear()
@@ -49,3 +47,35 @@ class ContractorPriceOne(MasterMaintenance):
             self.dryWidth.clear()
             self.dryLength.clear()
             self.dryPrice.clear()
+
+    def refresh_table(self, index=0):
+        if index == 0:
+            super().refresh_table()
+
+        elif index == 1:
+            self.model2 = tableModel(self.df2)
+            self.dryTableView.setModel(self.model2)
+
+    def construct_df(self, index):
+        # index 0 means wet wood, index 1 means dry wood
+        height = [str(self.wetHeight.text()), str(self.dryHeight.text())]
+        width = [str(self.wetWidth.text()), str(self.dryWidth.text())]
+        length = [str(self.wetLength.text()), str(self.dryLength.text())]
+        price = [str(self.wetPrice.text()), str(self.dryPrice.text())]
+
+        df = pd.DataFrame({'Size1': [height[index]], 'Size2': [width[index]],
+                           'Length': [length[index]], 'Price': [price[index]]})
+
+        return df
+
+    def add_df(self, index=0):
+        if index == 0:
+            super().add_df(self.construct_df(0))
+        elif index == 1:
+            # adding new data into the dataframe
+            tempDf = self.df2  # store the current data frame into a temp variable
+            self.df2 = tempDf.append(self.construct_df(1), ignore_index=True)  # adding new row into the dataframe
+            self.refresh_table(1)
+
+    def save_csv(self, index=0):
+        super().save_csv(self.csvFilePathList[index])
