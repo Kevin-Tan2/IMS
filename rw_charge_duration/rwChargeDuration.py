47073c14101081e8459ffa2af8838d78ddfa783c
Kevin Tan <kevintan314159@gmail.com>
2021-12-14 17:11:57 +1000

    RWChargeDuration can now sort the data by Range of Size

--- rw_charge_duration/rwChargeDuration.py
+++ rw_charge_duration/rwChargeDuration.py
@@ -12,10 +12,19 @@ class RWChargeDuration(MasterMaintenance):
 
         super().__init__(self.uiFilePath, self.csvFilePath, self.columnNames)
 
+        # sort by Range of Size ascending
+        self.df = self.df.sort_values(by='Range of Size')
+
     def reset_entries(self):
 
         self.size.clear()
         self.duration.clear()
+
+    def refresh_table(self):
+        # sort by Range of Size ascending
+        self.df = self.df.sort_values(by='Range of Size')
+
+        super().refresh_table()
 
     def construct_df(self):
 
