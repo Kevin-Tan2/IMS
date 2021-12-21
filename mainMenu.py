fb8720a6a26a7340b815eee54d2725d99beacf1d
Kevin Tan <kevintan314159@gmail.com>
2021-12-21 23:22:33 +1000

    Rename main.py into mainMenu.py, and added main menu ui

--- mainMenu.py
+++ mainMenu.py
@@ -1,5 +1,5 @@
 import sys
-
+import os
 from PyQt5.QtCore import QPropertyAnimation
 from login import login
 from PyQt5 import QtCore, QtWidgets
@@ -7,6 +7,17 @@ from PyQt5.uic import loadUi
 
 from A_master_maintenance_setup.master.storage_invoice.storageInvoiceNo import StorageInvoice
 from A_master_maintenance_setup.master.fw_cancel_no.fwCancelNo import FWCancel
+
+
+class MainMenu(QtWidgets.QMainWindow):
+    switch_window = QtCore.pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+
+        loadUi("mainMenu.ui", self)
+
+        self.buttonA.clicked.connect(lambda: self.switch_window.emit('A'))
 
 
 class MasterMaintenance(QtWidgets.QMainWindow):
@@ -74,22 +85,24 @@ class MasterMaintenance(QtWidgets.QMainWindow):
 class Controller:
 
     def __init__(self):
-        self.masterMaintenance = MasterMaintenance()
         self.loginPage = login.LoginPage("./login/login.ui", "./login/listUsers.csv")
+        self.mainMenu = MainMenu()
 
     def show_login(self):
         # display login page
         self.loginPage.show()
 
         # event response when the signal is emitted from the login page
-        self.loginPage.switch_window.connect(self.show_maintenance)
+        self.loginPage.switch_window.connect(self.show_main_menu)
 
-    def show_maintenance(self):
+    def show_main_menu(self):
         # close the login page
         self.loginPage.close()
 
+        # os.chdir("./A_master_maintenance_setup/master")
+
         # display the customer entry
-        self.masterMaintenance.show()
+        self.mainMenu.show()
 
 
 def main():
