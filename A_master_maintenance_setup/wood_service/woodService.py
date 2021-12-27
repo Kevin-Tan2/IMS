2326313d8edb7c7d91c4af6e2e7f3fde7b164599
Kevin Tan <kevintan314159@gmail.com>
2021-12-27 18:44:35 +1000

    Connect maximise and restore button to the toggle_max_restore

--- A_master_maintenance_setup/wood_service/woodService.py
+++ A_master_maintenance_setup/wood_service/woodService.py
@@ -5,6 +5,9 @@ from PyQt5.QtWidgets import QVBoxLayout, QSizeGrip
 from PyQt5.uic import loadUi
 from PyQt5 import QtCore, QtWidgets
 
+# Global value for the windows status
+WINDOW_SIZE = 0
+
 
 class WoodService(QtWidgets.QMainWindow):
     def __init__(self, currentDir=None):
@@ -30,9 +33,11 @@ class WoodService(QtWidgets.QMainWindow):
 
         # header's property
         self.closeBtn.clicked.connect(lambda: self.close())
+        self.maxBtn.clicked.connect(lambda: self.toggle_max_restore())
         self.header.mouseMoveEvent = self.moveWindow
 
     def mousePressEvent(self, event):
+        # Get the current position of the mouse
         self.oldPos = event.globalPos()
 
     def moveWindow(self, event):
@@ -42,6 +47,22 @@ class WoodService(QtWidgets.QMainWindow):
             delta = QPoint(event.globalPos() - self.oldPos)
             self.move(self.x() + delta.x(), self.y() + delta.y())
             self.oldPos = event.globalPos()
+
+    # Restore or maximize your window
+    def toggle_max_restore(self):
+        # Global windows state
+        global WINDOW_SIZE  # The default value is zero to show that the size is not maximized
+        win_status = WINDOW_SIZE
+
+        if win_status == 0:
+            # If the window is not maximized
+            WINDOW_SIZE = 1  # Update value to show that the window has been maxmized
+            self.showMaximized()
+
+        else:
+            # If the window is on its default size
+            WINDOW_SIZE = 0  # Update value to show that the window has been set to normal size
+            self.showNormal()
 
 
 # Create main function to test the module individually
