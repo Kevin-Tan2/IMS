28ea70f29cf580ab60b46196dee4dbc406400ee7
Kevin Tan <kevintan314159@gmail.com>
2022-01-03 11:12:41 +1000

    A17 Lorry Transport Charges can now load Its Ui

---
+++ A_master_maintenance_setup/wood_service/forest_wood/lorry_transport/lorryTransportCharges.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5 import QtWidgets
+import pandas as pd
+from masterMaintenance import MasterMaintenance
+from tableViewModel import tableModel, load_csv
+
+
+class LorryTransportCharges(MasterMaintenance):
+    def __init__(self, currentDir=None):
+
+        if currentDir is None:
+            currentDir = ""
+        else:
+            currentDir += "/"
+
+        self.uiFilePath = currentDir + "lorryTransportCharges.ui"
+        self.csvFilePath = currentDir + "lorryTransportCharges.csv"
+
+        # create headers for .csv use
+        self.columnNames = ['Customer ID', 'Lorry No', 'Price', 'Type']
+
+        super().__init__(self.uiFilePath, self.csvFilePath, self.columnNames)
+
+
+# to test each module
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    widget = LorryTransportCharges()
+    widget.show()
+    sys.exit(app.exec_())
