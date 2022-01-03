a3bba6f422871fb5465164c5b576e2a10858bcae
Kevin Tan <kevintan314159@gmail.com>
2022-01-03 11:15:48 +1000

    A17 Lorry Transport Charges can now save the current table to csv file by clicking on the save button

--- A_master_maintenance_setup/wood_service/forest_wood/lorry_transport/lorryTransportCharges.py
+++ A_master_maintenance_setup/wood_service/forest_wood/lorry_transport/lorryTransportCharges.py
@@ -49,6 +49,9 @@ class LorryTransportCharges(MasterMaintenance):
     def add_df(self):
         super().add_df(self.construct_df())
 
+    def save_csv(self):
+        super().save_csv(self.csvFilePath)
+
 
 # to test each module
 if __name__ == "__main__":
