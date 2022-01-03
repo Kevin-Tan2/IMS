885d1164b59829ab736eb103ab898a5d4f1a5199
Kevin Tan <kevintan314159@gmail.com>
2022-01-03 12:36:57 +1000

    A22 Container Loading now can add data to table by clicking the add button

--- A_master_maintenance_setup/wood_service/rubber_wood/container_loading/containerLoading.py
+++ A_master_maintenance_setup/wood_service/rubber_wood/container_loading/containerLoading.py
@@ -25,6 +25,26 @@ class ContainerLoading(MasterMaintenance):
         self.customerID.clear()
         self.containerCharges.clear()
 
+    def construct_df(self):
+        # construct a new data frame from the QLineEdits
+        customerID = str(self.customerID.text())
+        containerCharges = str(self.containerCharges.text())
+        woodType = None
+
+        if self.tonnageBtn.isChecked():
+            woodType = str(self.tonnageBtn.text())
+
+        elif self.tripBtn.isChecked():
+            woodType = str(self.tripBtn.text())
+
+        df = pd.DataFrame({'Customer ID': [customerID],
+                           'Container Charges': [containerCharges],
+                           'Type': [woodType]})
+        return df
+
+    def add_df(self):
+        super().add_df(self.construct_df())
+
 
 # to test each module
 if __name__ == "__main__":
