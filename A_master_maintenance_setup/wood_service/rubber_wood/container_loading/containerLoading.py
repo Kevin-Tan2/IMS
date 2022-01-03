0b856461de636ed59bd1d6d80c4f9adc79ef2249
Kevin Tan <kevintan314159@gmail.com>
2022-01-03 12:36:23 +1000

    A22 Container Loading now can reset entries by clicking the reset button

--- A_master_maintenance_setup/wood_service/rubber_wood/container_loading/containerLoading.py
+++ A_master_maintenance_setup/wood_service/rubber_wood/container_loading/containerLoading.py
@@ -20,6 +20,11 @@ class ContainerLoading(MasterMaintenance):
 
         super().__init__(self.uiFilePath, self.csvFilePath, self.columnNames)
 
+    def reset_entries(self):
+        # clear out the entries
+        self.customerID.clear()
+        self.containerCharges.clear()
+
 
 # to test each module
 if __name__ == "__main__":
