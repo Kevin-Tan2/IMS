7e51f32bc9af3d03821bf5c7b317db8907d66d9e
Kevin Tan <kevintan314159@gmail.com>
2022-01-04 11:09:33 +1000

    A25 Export Invoice No can now load UI widget

---
+++ A_master_maintenance_setup/export_charge/export_no/exportInvoice.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5 import QtWidgets
+import pandas as pd
+from masterMaintenance import InvoiceWidget
+
+
+class ExportInvoice(InvoiceWidget):
+    def __init__(self, currentDir=None):
+
+        if currentDir is None:
+            currentDir = ""
+        else:
+            currentDir += "/"
+
+        self.uiFilePath = currentDir + "exportInvoice.ui"
+        self.csvFilePath = currentDir + "exportInvoice.csv"
+
+        # create headers for .csv use
+        self.columnNames = ['Invoice No']
+
+        super().__init__(self.uiFilePath, self.csvFilePath, self.columnNames)
+
+
+# to test each module
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    widget = ExportInvoice()
+    widget.show()
+    sys.exit(app.exec_())
