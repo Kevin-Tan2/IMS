9dd2dbcc9900f947d205249c6224bfda892364d8
Kevin Tan <kevintan314159@gmail.com>
2022-01-04 11:01:25 +1000

    A24 Charges by Size can now save the current table to csv file by clicking the save button

--- A_master_maintenance_setup/export_charge/charges_by_size/sizeCharges.py
+++ A_master_maintenance_setup/export_charge/charges_by_size/sizeCharges.py
@@ -43,6 +43,9 @@ class SizeCharge(MasterMaintenance):
     def add_df(self):
         super().add_df(self.construct_df())
 
+    def save_csv(self):
+        super().save_csv(self.csvFilePath)
+
 
 # to test each module
 if __name__ == "__main__":
