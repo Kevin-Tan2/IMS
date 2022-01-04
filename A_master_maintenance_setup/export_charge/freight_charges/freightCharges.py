105c745d9f491d38678ec8a11d5fd24c44bcba93
Kevin Tan <kevintan314159@gmail.com>
2022-01-04 10:46:01 +1000

    A23 Freight Charge can now reset data entries by clicking the reset button

--- A_master_maintenance_setup/export_charge/freight_charges/freightCharges.py
+++ A_master_maintenance_setup/export_charge/freight_charges/freightCharges.py
@@ -20,6 +20,11 @@ class FreightCharges(MasterMaintenance):
 
         super().__init__(self.uiFilePath, self.csvFilePath, self.columnNames)
 
+    def reset_entries(self):
+        # clear out the entries
+        self.dischargePort.clear()
+        self.containerCharge.clear()
+
 
 # to test each module
 if __name__ == "__main__":
